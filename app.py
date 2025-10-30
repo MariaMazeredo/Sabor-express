e03a3c45801dc976844d7a3c13e95ec70f2ee745
Maria Mazeredo <dudamazeredo36@gmail.com>
2025-10-30 14:31:09 -0300

    Atualiza funções de menu e layout

--- app.py
+++ app.py
@@ -1,23 +1,133 @@
 import os
 
-print('𝕊𝕒𝕓𝕠𝕣 𝔼𝕩𝕡𝕣𝕖𝕤𝕤\n')
+restaurantes = [
+    {'nome': 'Lagostine', 'categoria': 'Frutos do mar', 'ativo': 'False'},
+    {'nome': 'Doce Sabor', 'categoria': 'Doces e sobremesas', 'ativo': 'True'},
+    {'nome': 'Napolini', 'categoria': 'Italiana', 'ativo': 'True'},
+]
 
-print('1. Cadastrar restaurante')
-print('2. Listar restaurantes')
-print('3. Ativar/Desativar restaurante')
-print('4. Sair\n')
+def exibir_nome_do_programa():
+    '''
+    Exibe o nome do programa com um print
+    
+    '''
+    print('𝕊𝕒𝕓𝕠𝕣 𝔼𝕩𝕡𝕣𝕖𝕤𝕤\n')
 
-opcao_escolhida = int(input('Escolha a opção desejada:'))
+def exibir__opcoes():
+    ''' Função responsável por exibir as opções do menu'''
+
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurantes')
+    print('3. Alterar status do restaurante')
+    print('4. Sair\n')
 
 def finalizar_app():
-    os.system('clear') # limpa a tela
-    print('Encerrando o aplicativo. Até logo!\n')
+    ''' Função que finaliza o app chamando exibir_subtitulo'''
+    exibir_subtitulo('Finalizar app')
 
-if opcao_escolhida == 1:
-    print('Cadastrar restaurante')
-elif opcao_escolhida == 2:
-    print('Listar restaurantes')
-elif opcao_escolhida == 3:
-    print('Ativar/Desativar restaurante')
-else:
-    finalizar_app()
+def voltar_ao_menu_principal():
+    input('\nDigite uma tecla para voltar ao menu ')
+    return # Retorna para a função que chamou esta função
+
+def opcao_invalida():
+    print('Opção inválida!\n')
+    voltar_ao_menu_principal()
+
+def exibir_subtitulo(texto):
+    ''' Essa função limpa o terminal após a execução, exibe o subtitulo daquela seção contornado com *'''
+    os.system('clear')
+    linha = '*' * (len(texto) + 4)
+    print(linha)
+    print(texto)
+    print(linha)
+    print()
+
+    
+def cadastrar_novo_restaurante():
+   '''Essa função é responsável por cadastrar um novo restaurante
+   
+   Inputs:
+   - Nome do restaurante
+   - Categoria
+
+   Output:
+   -adiciona um novo restaurante a lista de restaurantes
+   
+   '''
+   exibir_subtitulo('Cadastrar novo restaurante')
+   nome_restaurante = input('Digite o nome do restaurante: ')
+   categoria = input(f'Digite a categoria do restaurante {nome_restaurante}: ')
+   dados_restaurante = {'nome': nome_restaurante, 'categoria': categoria, 'ativo': 'False'}
+   restaurantes.append(dados_restaurante)
+   print(f'O restaurante {nome_restaurante} foi cadastrado com sucesso!\n')
+
+   voltar_ao_menu_principal()
+
+def alterar_status_restaurante():
+    '''Essa função é responsável por alterar o status do restaurante para ativo ou inativo'''
+    exibir_subtitulo('Alternar status do restaurante')
+    nome_restaurante = input('Digite o nome do restaurante que deseja alterar o status: ')
+    restaurante_encontrado = False
+
+    for restaurante in restaurantes:
+        if nome_restaurante == restaurante['nome']:
+            restaurante_encontrado = True
+            restaurante['ativo'] = not restaurante['ativo']
+            mensagem = f'O restaurante {nome_restaurante} foi ativado com sucesso' if restaurante['ativo'] else f'O restaurante {nome_restaurante} foi desativado com sucesso'
+            print(mensagem)
+            break  # Importante: interrompe o loop assim que o restaurante é encontrado
+
+    if not restaurante_encontrado:
+        print(f'O restaurante {nome_restaurante} não foi encontrado na lista.\n')
+    
+    voltar_ao_menu_principal()
+
+def listar_restaurantes():
+    '''Essa função é responsável por exibir uma lista com todos os restaurantes cadastrados expecificando nome, categoria e se está ativo'''
+    exibir_subtitulo('Lista de restaurantes cadastrados')
+
+    print(f'{'|Nome do Restaurante'.ljust(20)} | {'Categoria'.ljust(20)} | Status')
+    for restaurante in restaurantes:
+        nome_restaurante = restaurante['nome']
+        categoria = restaurante['categoria']
+        ativo = 'ativado' if restaurante['ativo'] else 'desativado'
+        status = 'Ativo' if ativo else 'Inativo'  # Exibe "Ativo" ou "Inativo" corretamente
+        print(f'- {nome_restaurante.ljust(20)} | {categoria.ljust(20)} | Status: {status}')
+
+    voltar_ao_menu_principal()
+
+def escolher_opcao():
+    '''Essa função é responsável pela lógica na escolha de opções do menu direcioando conforme escolha do user'''
+    try:
+        opcao_escolhida = int(input('Escolha a opção desejada:'))
+
+        if opcao_escolhida == 1:
+            cadastrar_novo_restaurante()
+        elif opcao_escolhida == 2:
+            listar_restaurantes()
+        elif opcao_escolhida == 3:
+            alterar_status_restaurante()
+        elif opcao_escolhida == 4:
+            finalizar_app()
+        else:
+            opcao_invalida()
+    except:
+        opcao_invalida()
+
+def main():
+    '''
+    Função principal do programa.
+
+    Exibe continuamente o nome do programa e o menu de opções,
+    aguardando a escolha do usuário. O loop principal mantém o 
+    programa em execução até que o usuário selecione a opção de saída.
+    
+    '''
+    while True:
+        exibir_nome_do_programa()
+        exibir__opcoes()
+        escolher_opcao()
+    
+
+if __name__ == '__main__':
+    main()
---
+++ exercicios.py
@@ -0,0 +1,176 @@
+""" """ #1- Solicite ao usuário que insira um número e, em seguida, use uma estrutura if else para determinar se o número é par ou ímpar.
+
+numero = int(input("Digite um número: "))
+if numero % 2 == 0:
+    print("O número digitado é par")
+else:
+    print("O número digitado é ímpar")
+
+#2- Pergunte ao usuário sua idade e, com base nisso, use uma estrutura if elif else para classificar a idade em categorias de acordo com as seguintes condições:Criança: 0 a 12 anos;Adolescente: 13 a 18 anos;Adulto: acima de 18 anos.
+
+idade = int(input("Digite sua idade: "))
+if idade  >=0 and idade <=12:
+    print("Criança")
+elif idade >=13 and idade <=18:
+    print("Adolescente")
+elif idade >18 and idade <=120:
+    print("Adulto")
+else:
+    print("Múmia")
+
+   # Solicite um nome de usuário e uma senha e use uma estrutura if else para verificar se o nome
+   # de usuário e a senha fornecidos correspondem aos valores esperados determinados por você.
+
+from getpass import getpass
+import re
+
+#3 - Solicite um nome de usuário e uma senha e use uma estrutura if else para verificar se o nome de usuário e a senha fornecidos correspondem aos valores esperados determinados por você.
+senha_valida="Python123"
+nome_user_valido = "Maria"
+
+nome_user_valido= input("Digite o nome de usuário: ")
+if nome_user_valido == "Maria" :
+    print("Bem-vinda, Maria! insira a senha para continuar.")
+else:
+    print("Nome de usuário inválido.")
+
+senha_valida = input("Digite a senha: ")
+if senha_valida == "Python123":
+    print("Acesso concedido.")
+else:
+    print("Senha incorreta. Acesso negado.")
+
+#4
+coordenadas_x = float(input("Digite a coordenada x: "))
+coordenadas_y = float(input("Digite a coordenada y: "))
+
+if coordenadas_x > 0 and coordenadas_y > 0:
+    print("O ponto está no primeiro quadrante.")
+elif coordenadas_x < 0 and coordenadas_y >0:
+    print("O ponto está no segundo quadrante.")
+elif coordenadas_x < 0 and coordenadas_y < 0:
+    print("O ponto está no terceiro quadrante.")
+elif coordenadas_x > 0 and coordenadas_y < 0:
+    print("O ponto está no quarto quadrante.")
+else:
+    print("O ponto está na origem ou em um dos eixos.")
+
+#1 - Crie uma lista para cada informação a seguir:Lista de números de 1 a 10;Lista com quatro nomes;Lista com o ano que você nasceu e o ano atual.
+
+lista_numeros = [1,2,3,4,5,6,7,8,9,10]
+lista_nomes = ['Regina', 'Regiane', 'Reinaldo', 'Rejane']
+lista_anos = [2004, 2025]
+
+#2 - Crie uma lista e utilize um loop for para percorrer todos os elementos da lista.
+
+lista_de_compras = ['arroz', 'frango', 'requeijão', 'uvas']
+for item in lista_de_compras:
+    print(item)
+
+#3 - Utilize um loop for para calcular a soma dos números ímpares de 1 a 10.
+
+numeros_impares = [1,3,5,7,9]
+soma = 0
+for numero in numeros_impares:
+    soma += numero
+print("A soma dos números ímpares de 1 a 10 é:", soma)
+
+#4 - Utilize um loop for para imprimir os números de 1 a 10 em ordem decrescente.
+numeros= [10,9,8,7,6,5,4,3,2,1]
+for numero in numeros:
+    print(numero)
+
+#outra possibilidade
+
+for numero in range(10, 0, 1):
+    print(numero)
+
+#5 - Solicite ao usuário um número e, em seguida, utilize um loop for para imprimir a tabuada desse número, indo de 1 a 10.
+
+numero_tabuada =int(input("Digite um número para consultar a tabuada: "))
+for numero in range(1,11):
+    resultado = numero_tabuada * numero
+    print(f'{numero_tabuada} X {numero} = {resultado}')
+
+#6 - Crie uma lista de números e utilize um loop for para calcular a soma de todos os elementos. Utilize um bloco try-except para lidar com possíveis exceções.
+
+lista_numeros = [20, 10, 30, 60]
+for numero in lista_numeros:
+    try:
+        soma += numero
+        print(f'O resultado da soma é: {soma}')
+    except TypeError:
+        print(f'Erro ao somar o valor: {numero}. Certifique-se de que todos os elementos são números.')
+
+#7 - Construa um código que calcule a média dos valores em uma lista. Utilize um bloco try-except para lidar com a divisão por zero, caso a lista esteja vazia.
+
+lista_valores=[50, 21, 22, 23, 49]
+
+try:
+    media = sum(lista_valores) / len(lista_valores)
+    print(f'A média dos valores na lista é: {media}')
+except ZeroDivisionError:
+    print('Erro: A lista está vazia. Não é possível calcular a média.')
+
+#1 - Crie um dicionário representando informações sobre uma pessoa, como nome, idade e cidade.
+
+pessoa = {
+    "nome": "Maria Do Rosário",
+    "idade": 25,
+    "cidade": "Ipiraporinha do Oeste",
+    "profissão": "Mecânica de trens"
+}
+
+#2 - Utilizando o dicionário criado no item 1:
+
+""" Modifique o valor de um dos itens no dicionário (por exemplo, atualize a idade da pessoa);
+Adicione um campo de profissão para essa pessoa;
+Remova um item do dicionário. """
+
+pessoa["idade"] = 26
+pessoa["Nome da mãe"] = "Josefina"
+pessoa.pop("profissão")
+
+# 3 - Crie um dicionário que relacione os números de 1 a 5 aos seus respectivos quadrados.
+
+quadrados = {
+    1: 1,
+    2: 4,
+    3: 9,
+    4: 16,
+    5: 25
+}
+
+#4 - Crie um dicionário e verifique se uma chave específica existe dentro desse dicionário.
+
+gatos = {
+    "gato1": "Belzin",
+    "gato2": "Mimo",
+    "gato3": "Chan"
+}
+
+if "gato5" in gatos:
+    print("A chave gato5 existe no dicionário.")
+else:
+    print("A chave gato5 não existe no dicionário.")
+
+#5 - Escreva um código que conte a frequência de cada palavra em uma frase utilizando um dicionário.
+
+frase = input("Digite uma frase: ")
+palavras = frase.split()
+frequencia = {}
+for palavra in palavras:
+    if palavra in frequencia:
+        frequencia[palavra] += 1     # já existe → soma +1
+    else:
+        frequencia[palavra] = 1      # primeira vez → define 1
+        print(frequencia)
+
+# ou podemos fazer:
+
+frase = "Python se tornou uma das linguagens de programação mais populares do mundo nos últimos anos."
+contagem_palavras = {}
+palavras = frase.split()
+for palavra in palavras:
+    contagem_palavras[palavra] = contagem_palavras.get(palavra, 0) + 1
+print(contagem_palavras)
